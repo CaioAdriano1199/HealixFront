e82d5a8db4126512e4d71ee6373a38a2924c77a0
CaioAdriano1199 <caioadriano1199@gmail.com>
2026-02-02 16:50:42 -0300

    feita a tela de medicamento

--- healix/src/app/home/page.js
+++ healix/src/app/home/page.js
@@ -3,6 +3,7 @@ import React from "react";
 import Button from "../components/button/button";
 import Forms from "../components/forms/forms";
 import Input from "../components/input/input";
+import Textarea from "../components/textarea/textarea";
 import { useState, useEffect } from "react";
 
 
@@ -10,6 +11,7 @@ export default function Home() {
   const [isDesktop, setIsDesktop] = useState(false);
   const [conttela, setConttela] = useState(0);
   const [lembrecard, setLembrecard] = useState([]);
+  const [istemp, setIstemp] = useState(false);
 
   useEffect(() => {
     const checkScreen = () => setIsDesktop(window.innerWidth >= 1024);
@@ -76,7 +78,7 @@ export default function Home() {
             <h2 className="text-4xl md:text-5xl lg:text-3xl font-bold mb-10 lg:mb-4  mt-4 text-[var(--azulescuro)]">Healix</h2>
             <div className="flex flex-col h-full gap-4 p-4 w-full">
               {lembrecard.map((cards) => (
-                <Forms>
+                <Forms key={cards.id}>
                   <h2 className="text-lg font-semibold">{cards.titulo}</h2>
                   <p className="text-sm text-gray-600 mt-2">{cards.descricao}</p>
                 </Forms>
@@ -84,8 +86,24 @@ export default function Home() {
             </div>
           </div>)}
         {conttela === 1 && ( // Tela Medicamentos
-          <div className="flex flex-col items-center justify-center h-screen pt-16 pb-12 bg-[var(--fundobranco)]">
-            <h2 className="text-2xl font-bold mb-4">Medicamentos</h2>
+          <div className="flex flex-col items-center justify-center h-screen  lg:pt-16 lg:mt-16 mb-12 pb-12 bg-[var(--fundobranco)]">
+            <h1 className="text-4xl font-bold mb-4 lg:mt-10 text-[var(--azulescuro)]">Healix</h1>
+            <h3 className="text-xl font-bold mb-4 text-[var(--azulescuro)]">Cadastre seu medicamento aqui:</h3>
+            <Forms className="flex flex-col items-center mt-6 w-full max-w-[300px] p-4">
+              <Input type="text" texto={"Inserir Medicamento"} placeholder="Ex: Dipirona" className="mb-4 p-2 border border-[var(--cinza)] rounded" />
+              <Textarea texto={"Descrição (opcinal)"} placeholder="Inserir descrição" className="mb-4 p-2 border border-[var(--cinza)] rounded" />
+              <Input texto={"Horario do medicamento"} type="text" placeholder="Frequência" className="mb-4 p-2 border border-[var(--cinza)] rounded" />
+              <div className="flex items-center justify-around w-full ">
+                <input type="checkbox" onChange={(e) => setIstemp(e.target.checked)} className="mb-4 accent-[var(--azulescuro)]" />
+                <label className="mb-4">Medicamento de uso temporario</label>
+              </div>
+              <>
+              {istemp === true && (
+                <Input texto={"Tempo de uso (em dias)"} type="number" placeholder="Ex: 7" className="mb-4 p-2 border border-[var(--cinza)] rounded" />
+              )}
+              </>
+              <Button type="submit" className="bg-[var(--azulescuro)] text-[var(--fundobranco)] w-full p-2 rounded">Cadastrar</Button>
+            </Forms>
           </div>)}
         {conttela === 2 && ( // Tela Perfil
           <div className="flex flex-col items-center justify-center h-screen pt-16 pb-12 bg-[var(--fundobranco)]">
